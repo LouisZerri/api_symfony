c126ccbe937ee068834f12a0dc0a26e6e84ac81a
Louis <l.zerri@pepperbay.fr>
2020-03-10 12:34:30 +0100

    feat: fin partie api platform

---
+++ src/Events/CustomerUserSubscriber.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Events;
+
+use Symfony\Component\HttpKernel\KernelEvents;
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\Customer;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\Security\Core\Security;
+
+class CustomerUserSubscriber implements EventSubscriberInterface
+{
+
+    /**
+     * @var Security
+     */
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['setUserForCustomer', EventPriorities::PRE_VALIDATE]
+        ];
+    }
+
+    public function setUserForCustomer(ViewEvent $event)
+    {
+        $result = $event->getControllerResult();
+
+        $method = $event->getRequest()->getMethod();
+
+        if($result instanceof Customer && $method === "POST")
+        {
+            $user = $this->security->getUser();
+
+            $result->setUser($user);
+        }
+
+
+    }
+
+}
---
+++ src/Events/InvoiceChronoSubscriber.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Events;
+
+use App\Entity\Invoice;
+use App\Repository\InvoiceRepository;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use ApiPlatform\Core\EventListener\EventPriorities;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+class InvoiceChronoSubscriber implements EventSubscriberInterface
+{
+
+    /**
+     * @var Security
+     */
+    private $security;
+
+    /**
+     * @var InvoiceRepository
+     */
+    private $repository;
+
+    public function __construct(Security $security, InvoiceRepository $repository)
+    {
+        $this->security = $security;
+        $this->repository = $repository;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['setChronoForInvoice', EventPriorities::PRE_VALIDATE]
+
+        ];
+    }
+
+    public function setChronoForInvoice(ViewEvent $event)
+    {
+        $invoice = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        if ($invoice instanceof Invoice && $method === "POST") 
+        {
+            $user = $this->security->getUser();
+
+            if ($user !== $invoice->getCustomer()->getUser()) 
+            {
+                throw new AccessDeniedHttpException("Ce client ne vous appartient pas");
+            }
+
+            $invoice->setChrono($this->repository->findNextChrono($user));
+
+            if(empty($invoice->getSentAt()))
+            {
+                $invoice->setSentAt(new \DateTime());
+            }
+        }
+
+    }
+}
