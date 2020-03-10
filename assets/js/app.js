8e762fbdb8f836744b5f1807a2cb07eddc1797f1
Louis <l.zerri@pepperbay.fr>
2020-03-10 13:38:29 +0100

    feat: mise en place de react

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import '../css/app.css';
+
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+const App = () => {
+    return <h1>Bonjour à tous !</h1>
+}
+
+const rootElement = document.querySelector('#app');
+ReactDOM.render(<App />, rootElement);
